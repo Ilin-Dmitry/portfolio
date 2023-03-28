c2f7a68011d16cd98b772998c6e64d779a1bad69
Ilin-Dmitry <dm-ilin@mail.ru>
2023-03-28 08:09:44 +0400

    Style links hover effect

--- scripts/dataChange.js
+++ scripts/dataChange.js
@@ -1,11 +1,5 @@
 const projectNumbers = document.querySelectorAll('.project__number');
-const projectsArray = document.querySelectorAll('.project');
 
 projectNumbers.forEach((number, i) => {
     number.textContent = '0' + (i + 1);
-})
-
-projectsArray.forEach(project => {
-    const img = project.querySelector('.project__img')
-    console.log('img =>', img)
 })
